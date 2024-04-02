165b01d92167cd8cebe2167ad9f308652381b120
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-04-02 17:31:41 +0530

    intaialize backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('conneted to database');
+
+}).catch((err) => {
+    console.log('connection failed');
+})
 const app = express();
 
 app.listen(3000,() => {
     console.log('Server is running on port 3000!')
 });
+
