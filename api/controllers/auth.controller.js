d06b3a3b823ece390dcddc7d83eac46b9eb98794
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-04-05 01:18:31 +0530

    added middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcrptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcrptjs.hashSync(password, 10);
     const newUser = new User ({username, email, password: hashedPassword });
@@ -10,7 +11,8 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json('user created successfully');
     } catch(error) {
-        res.status(404).json(error.message); //gives error as duplicate key to user 
+        // res.status(404).json(error.message); //gives error as duplicate key to user 
+        next(error);
     }
 
 };
