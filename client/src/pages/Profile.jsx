a8e2cd8b278a96b54e925ce92567604f5eec2d9f
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-04-07 01:51:09 +0530

    do not added duntionality to update button, delete account and sign out will add it later

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ export default function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto gap-4">
       <h1 className="text-3xl  font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col"> 
+      <form className="flex flex-col gap-3"> 
         <input type="text" placeholder="username" id="userName" className="border p-3 rounded-lg"/>
         <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg"/>
         <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg"/>
